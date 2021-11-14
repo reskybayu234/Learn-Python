948fcb773828cca743be3169ee9b9455634085f3
Resky Bayu Andhika <75589544+reskybayu234@users.noreply.github.com>
2021-11-14 13:58:34 +0700

    Add files via upload

---
+++ episode 03 - Alur Program Python/Main.py
@@ -0,0 +1,11 @@
+import time
+start_time = time.time()
+
+print("bayu")
+print("hello")
+print("world")
+print("Hello World")
+a=6
+print(a)
+print(time.time()-start_time,"detik")
+#alur program sesuai dengan urutan baris program
