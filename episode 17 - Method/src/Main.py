b7eeca044bc28181b91ac14dce5572e72ed5b9eb
Resky Bayu Andhika <75589544+reskybayu234@users.noreply.github.com>
2021-11-20 19:55:42 +0700

    Add files via upload

---
+++ episode 17 - Method/src/Main.py
@@ -0,0 +1,16 @@
+# class
+class Mahasiswa():
+    nama = 'nama'
+
+    def belajar(self):
+        print(self.nama,"sedang belajar")
+
+# main program
+
+bayu = Mahasiswa()
+resky = Mahasiswa()
+
+bayu.nama = "bayu andhika"
+resky.nama = "resky bayu"
+
+bayu.belajar()
